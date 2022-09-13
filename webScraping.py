463ac7a4a1eee8f6e1f6c478f680a42341204098
Alvin <alvin.tol@hotmail.com>
2022-09-13 11:19:04 -0600

    adds average rating/price variables

--- webScraping.py
+++ webScraping.py
@@ -7,14 +7,21 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 items = soup.findAll(class_='col-sm-4 col-lg-4 col-md-4')
 
+avgPrice = []
+avgRating = []
+
 for item in items:
   # print('-------ITEM-------:', '\n', item)
   
-  # price = item.find(class_='pull-right price').get_text()
-  # print('-------PRICE-------:', '\n', price)
+  price = item.find(class_='pull-right price').get_text()
+  avgPrice.append(price)
+  print('-------PRICE-------:', '\n', price)
+  print('-------AVERAGE PRICE-------:', '\n', avgPrice)
   
   # link = item.find('a')['href']
   # print('-------LINK-------:', '\n', link)
   
   rating = item.find(attrs={'data-rating': True})
-  print('-------LINK-------:', '\n', len(rating))
+  avgRating.append(len(rating))
+  print('-------RATING-------:', '\n', len(rating))
+  print('-------AVERAGE RATING-------:', '\n', avgRating)
