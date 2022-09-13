04dcc25993dafd4c830d95b388c8457a8746ad59
Alvin <alvin.tol@hotmail.com>
2022-09-13 10:47:21 -0600

    adds find all to retrieve all three item posts

--- webScraping.py
+++ webScraping.py
@@ -4,3 +4,8 @@ from csv import writer
 
 response = requests.get('https://www.webscraper.io/test-sites/e-commerce/allinone')
 soup = BeautifulSoup(response.text, 'html.parser')
+
+items = soup.findAll(class_='col-sm-4 col-lg-4 col-md-4')
+
+for item in items:
+  print('-------ITEM-----:', '\n', item)
