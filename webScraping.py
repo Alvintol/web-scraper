5290e21a2e8ea84ae0f06a7505a81ed44b571855
Alvin <alvin.tol@hotmail.com>
2022-09-13 10:53:50 -0600

    find all links/hrefs

--- webScraping.py
+++ webScraping.py
@@ -9,6 +9,9 @@ items = soup.findAll(class_='col-sm-4 col-lg-4 col-md-4')
 
 for item in items:
   # print('-------ITEM-------:', '\n', item)
-  price = item.find(class_='pull-right price').get_text()
-  print('-------PRICE-------:', '\n', price)
   
+  # price = item.find(class_='pull-right price').get_text()
+  # print('-------PRICE-------:', '\n', price)
+  
+  link = item.find('a')['href']
+  print('-------LINK-------:', '\n', link)
