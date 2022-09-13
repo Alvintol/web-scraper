9755d33dfa2336d172276ee1157628d19b60fef9
Alvin <alvin.tol@hotmail.com>
2022-09-13 11:22:00 -0600

    imports functools for reduce method

--- webScraping.py
+++ webScraping.py
@@ -1,8 +1,10 @@
 import requests
+import functools
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get('https://www.webscraper.io/test-sites/e-commerce/allinone')
+response = requests.get(
+    'https://www.webscraper.io/test-sites/e-commerce/allinone')
 soup = BeautifulSoup(response.text, 'html.parser')
 
 items = soup.findAll(class_='col-sm-4 col-lg-4 col-md-4')
@@ -11,17 +13,18 @@ avgPrice = []
 avgRating = []
 
 for item in items:
-  # print('-------ITEM-------:', '\n', item)
-  
-  price = item.find(class_='pull-right price').get_text()
-  avgPrice.append(price)
-  print('-------PRICE-------:', '\n', price)
-  print('-------AVERAGE PRICE-------:', '\n', avgPrice)
-  
-  # link = item.find('a')['href']
-  # print('-------LINK-------:', '\n', link)
-  
-  rating = item.find(attrs={'data-rating': True})
-  avgRating.append(len(rating))
-  print('-------RATING-------:', '\n', len(rating))
-  print('-------AVERAGE RATING-------:', '\n', avgRating)
+    # print('-------ITEM-------:', '\n', item)
+
+    price = item.find(class_='pull-right price').get_text()
+    avgPrice.append(price)
+    print('-------PRICE-------:', '\n', price)
+    print('-------AVERAGE PRICE-------:', '\n', avgPrice)
+
+    # link = item.find('a')['href']
+    # print('-------LINK-------:', '\n', link)
+
+    rating = item.find(attrs={'data-rating': True})
+    avgRating.append(len(rating))
+    print('-------RATING-------:', '\n', len(rating))
+    print('-------AVERAGE RATING-------:', '\n',
+          avgRating.reduce(lambda a, b: a+b, avgRating))
