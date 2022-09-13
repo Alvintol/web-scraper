ac750130284be6e364aca86bc51157525e28dba9
Alvin <alvin.tol@hotmail.com>
2022-09-13 10:42:04 -0600

    adds soup variable parsing get request response

--- webScraping.py
+++ webScraping.py
@@ -3,3 +3,4 @@ from bs4 import BeautifulSoup
 from csv import writer
 
 response = requests.get('https://www.webscraper.io/test-sites/e-commerce/allinone')
+soup = BeautifulSoup(response.text, 'html.parser')
